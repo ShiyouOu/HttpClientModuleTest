3c82bde9ba4418875b81228f3e3dfac786fd37bd
ShiyouOu <54868441+ShiyouOu@users.noreply.github.com>
2022-12-11 00:04:22 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { AppRoutingModule } from './app.routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    CommonModule,
+    AppRoutingModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
